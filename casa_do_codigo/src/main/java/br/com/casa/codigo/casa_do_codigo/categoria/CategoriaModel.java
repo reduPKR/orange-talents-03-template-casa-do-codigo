4e0d48ee8554e5491af06746cc422ba36d5a29ee
redu <reduardo19942@gmail.com>
2021-03-16 09:44:57 -0300

    model categorias adicionada

---
+++ casa_do_codigo/src/main/java/br/com/casa/codigo/casa_do_codigo/categoria/CategoriaModel.java
@@ -0,0 +1,23 @@
+package br.com.casa.codigo.casa_do_codigo.categoria;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "categorias")
+public class CategoriaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotNull
+    @Column(unique = true)
+    private String nome;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
