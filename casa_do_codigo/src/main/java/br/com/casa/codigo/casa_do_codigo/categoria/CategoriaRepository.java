7369df3ab10a60b78619e2cfeb317c7feea2b37f
redu <reduardo19942@gmail.com>
2021-03-16 09:46:55 -0300

    repository categorias adicionado

---
+++ casa_do_codigo/src/main/java/br/com/casa/codigo/casa_do_codigo/categoria/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package br.com.casa.codigo.casa_do_codigo.categoria;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
+}
