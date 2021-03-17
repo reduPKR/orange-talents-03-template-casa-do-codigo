b048058965ad48fb5063987a6e1b2eb5239345e7
redu <reduardo19942@gmail.com>
2021-03-17 19:57:07 -0300

    comecei a escrever a controller  de estados criada

--- casa_do_codigo/src/main/java/br/com/casa/codigo/casa_do_codigo/localizacao/estado/EstadoController.java
+++ casa_do_codigo/src/main/java/br/com/casa/codigo/casa_do_codigo/localizacao/estado/EstadoController.java
@@ -1,8 +1,12 @@
 package br.com.casa.codigo.casa_do_codigo.localizacao.estado;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
 
 @RestController
 @RequestMapping("/estados")
@@ -10,5 +14,9 @@ public class EstadoController {
     @Autowired
     private EstadoRepository estadoRepository;
 
-
+    @PostMapping
+    @Transactional
+    public ResponseEntity<EstadoDTO> cadastrar(EstadoForm estadoForm){
+        return ResponseEntity.badRequest().build();
+    }
 }
