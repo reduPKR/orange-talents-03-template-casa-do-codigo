fa9d087f71b84eb3a23c59c7b008de7aa90671c9
redu <reduardo19942@gmail.com>
2021-03-17 16:21:33 -0300

    0-0-7 detalhes livro finalizado

--- casa_do_codigo/src/main/java/br/com/casa/codigo/casa_do_codigo/livro/LivroController.java
+++ casa_do_codigo/src/main/java/br/com/casa/codigo/casa_do_codigo/livro/LivroController.java
@@ -2,6 +2,7 @@ package br.com.casa.codigo.casa_do_codigo.livro;
 
 import br.com.casa.codigo.casa_do_codigo.livro.dto.ListaLivroDTO;
 import br.com.casa.codigo.casa_do_codigo.livro.dto.LivroDTO;
+import br.com.casa.codigo.casa_do_codigo.livro.dto.LivroDetalheDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/livros")
@@ -30,5 +32,16 @@ public class LivroController {
     @GetMapping
     public List<ListaLivroDTO> listar(){
         return ListaLivroDTO.converter(livroRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LivroDetalheDTO> exibir(@PathVariable Long id){
+        Optional<LivroModel> optional = livroRepository.findById(id);
+
+        if(optional.isPresent()) {
+            LivroDetalheDTO livroDetalhe = new LivroDetalheDTO(optional.get());
+            return ResponseEntity.ok(livroDetalhe);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
--- casa_do_codigo/src/main/java/br/com/casa/codigo/casa_do_codigo/livro/dto/LivroDetalheDTO.java
+++ casa_do_codigo/src/main/java/br/com/casa/codigo/casa_do_codigo/livro/dto/LivroDetalheDTO.java
@@ -3,29 +3,26 @@ package br.com.casa.codigo.casa_do_codigo.livro.dto;
 import br.com.casa.codigo.casa_do_codigo.autor.AutorModel;
 import br.com.casa.codigo.casa_do_codigo.categoria.CategoriaModel;
 import br.com.casa.codigo.casa_do_codigo.livro.LivroModel;
+import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDate;
 
-public class LivroDTO {
+public class LivroDetalheDTO {
     private String titulo;
     private String resumo;
     private String sumario;
     private double preco;
     private int numeroPaginas;
     private String isbn;
-    private LocalDate publicacao;
-    private CategoriaModel categoria;
     private AutorModel autor;
 
-    public LivroDTO(LivroModel livro) {
+    public LivroDetalheDTO(LivroModel livro) {
         this.titulo = livro.getTitulo();
         this.resumo = livro.getResumo();
         this.sumario = livro.getSumario();
         this.preco = livro.getPreco();
         this.numeroPaginas = livro.getNumeroPaginas();
         this.isbn = livro.getIsbn();
-        this.publicacao = livro.getPublicacao();
-        this.categoria = livro.getCategoria();
         this.autor = livro.getAutor();
     }
 
@@ -51,14 +48,6 @@ public class LivroDTO {
 
     public String getIsbn() {
         return isbn;
-    }
-
-    public LocalDate getPublicacao() {
-        return publicacao;
-    }
-
-    public CategoriaModel getCategoria() {
-        return categoria;
     }
 
     public AutorModel getAutor() {
