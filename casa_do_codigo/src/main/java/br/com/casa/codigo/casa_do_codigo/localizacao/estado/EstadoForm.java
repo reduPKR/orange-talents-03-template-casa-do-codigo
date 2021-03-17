c1d8b53b3d656b4486d0c0fd2abe6239df31e761
redu <reduardo19942@gmail.com>
2021-03-17 20:43:16 -0300

    Dei rollback, vou pensar melhor como modificar o annotation

--- casa_do_codigo/src/main/java/br/com/casa/codigo/casa_do_codigo/localizacao/estado/EstadoForm.java
+++ casa_do_codigo/src/main/java/br/com/casa/codigo/casa_do_codigo/localizacao/estado/EstadoForm.java
@@ -1,14 +1,19 @@
 package br.com.casa.codigo.casa_do_codigo.localizacao.estado;
 
 import br.com.casa.codigo.casa_do_codigo.localizacao.pais.PaisModel;
+import br.com.casa.codigo.casa_do_codigo.validador.UniqueValue;
 
 import javax.validation.constraints.NotNull;
 
 public class EstadoForm {
     @NotNull
+    @UniqueValue(domainClass = EstadoModel.class, fieldName = "nome")
     private String nome;
     @NotNull
     private PaisModel pais;
+
+    public EstadoForm() {
+    }
 
     public String getNome() {
         return nome;
