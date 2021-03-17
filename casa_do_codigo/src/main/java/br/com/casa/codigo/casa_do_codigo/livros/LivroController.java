a11897f6f72815b1f993630b9387c4c737cc7715
redu <reduardo19942@gmail.com>
2021-03-16 21:32:13 -0300

    Controller de livros adicionado

---
+++ casa_do_codigo/src/main/java/br/com/casa/codigo/casa_do_codigo/livros/LivroController.java
@@ -0,0 +1,18 @@
+package br.com.casa.codigo.casa_do_codigo.livros;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+
+@RestController
+@RequestMapping("/livros")
+public class LivroController {
+    @PostMapping
+    @Transactional
+    public ResponseEntity<String> cadastrar(){
+        return null;
+    }
+}
