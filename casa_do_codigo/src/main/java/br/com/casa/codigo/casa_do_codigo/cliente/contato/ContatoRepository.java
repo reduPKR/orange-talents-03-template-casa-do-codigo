7860c004041672cfcad283ca7687ded05445ad13
redu <reduardo19942@gmail.com>
2021-03-18 14:52:34 -0300

    repository de contatos criada

---
+++ casa_do_codigo/src/main/java/br/com/casa/codigo/casa_do_codigo/cliente/contato/ContatoRepository.java
@@ -0,0 +1,6 @@
+package br.com.casa.codigo.casa_do_codigo.cliente.contato;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContatoRepository extends JpaRepository<ContatoModel, Long> {
+}
